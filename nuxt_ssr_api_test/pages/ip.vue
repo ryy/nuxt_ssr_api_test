006b64d13775121f64511f94958dae17e722add3
ryo.naruse <naruse19910801@gmail.com>
2020-11-09 14:47:56 +0900

    init

---
+++ nuxt_ssr_api_test/pages/ip.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ ip }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      ip: ""
+    }
+  },
+  async asyncData({ $axios }) { // asyncData という nuxt 特有の機能において
+    const ip = await $axios.$get('http://icanhazip.com')
+    return { ip }
+  }
+}
+</script>
